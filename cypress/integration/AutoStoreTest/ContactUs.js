24c365e89a0f81a2d50d8c3059b8421db717e060
vivek <avivek1990@gmail.com>
2021-11-19 00:51:28 +0530

    assert value update

--- cypress/integration/AutoStoreTest/ContactUs.js
+++ cypress/integration/AutoStoreTest/ContactUs.js
@@ -10,7 +10,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
         cy.get("button[title='Submit']").click();
-        cy.get('.mb40 > :nth-child(3)').should('have.text','your enquiry has been successfully sent to the store owner!')
+        cy.get('.mb40 > :nth-child(3)').should('have.text','Your enquiry has been successfully sent to the store owner!')
     });
     
 //cypress run --record --key 72ba9792-bf20-4d16-ae18-e17526026eb0
